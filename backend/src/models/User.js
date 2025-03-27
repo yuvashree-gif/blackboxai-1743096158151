1a8d9212b18fb47af3b86eda99f5aec38793d0bb
yuvashree-gif <yuviiyuvashree@gmail.com>
2025-03-27 22:53:11 +0530

    Add backend/src/models/User.js 1743096191921

---
+++ backend/src/models/User.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profilePicture: {
+    type: String,
+    default: ''
+  },
+  followers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  following: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  posts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Post'
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Middleware to hash password before saving
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await require('bcryptjs').genSalt(10);
+    this.password = await require('bcryptjs').hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Method to compare passwords
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  return await require('bcryptjs').compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
